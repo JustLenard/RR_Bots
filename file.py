93c3baa1480addc33ac8739d840c245f555a1f65
JustLenard <justlenard.justme@gmail.com>
2024-03-08 17:22:15 +0200

    added poetry

---
+++ file.py
@@ -0,0 +1,5 @@
+x = 2
+
+
+def do_stuff(x: str) -> None:
+    print(f"Hell {x}")
--- test.py
+++ test.py
@@ -1,6 +1,26 @@
 from dotenv import load_dotenv
+from selenium import webdriver
 import os
+import file
 
 load_dotenv()
 
 print(os.getenv("RR_EMAIL"))
+
+
+print(file.x)
+print(file.do_stuff("mate"))
+
+with open("chapter_links.txt", "r+") as f:
+    links_from_file = f.readlines()
+    links_from_file = [x.replace("\n", "") for x in links_from_file]
+
+    for link in links_array:
+
+        if link not in links_from_file:
+            new_links.append(link)
+            f.write(str(link) + "\n")
+
+    if len(links_from_file) > 500:
+        os.remove(links_file_name)
+    print(len(links_from_file))
